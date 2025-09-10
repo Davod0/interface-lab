a18807ab639b0636dae9c1184bfe58beb2b5c473
Davod <davodnikoyii@gmail.com>
2025-09-10 22:24:33 +0200

    Refactor HeroSection component layout and styling; improve responsiveness and add action buttons

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,32 +1,55 @@
+
 import React from "react";
 import Image from "next/image";
 
 const HeroSection = () => {
-  return (
-    <section>
-        <div className="grid grid-cols-1 lg:grid-cols-12">
-            <div className="col-span-7 place-self-center">
-                <h1 className="text-white mb-4 text-4xl
-                    lg:text-6xl font-extrabold">
-                    Hi I am Davod
-                </h1>
-                <p className="text-[#ADB7BE] text-lg lg:text-xl">
-                    I build things for the web ekfhwgietgwejgtw
-                    hieibns  rwnio ntg ni puhtgthgptutrwepn iour
-                </p>
+    return (
+        <section>
+            <div className="grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-7 place-self-center text-center sm:text-left">
+                    <h1
+                        className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold"
+                    >
+                        <span
+                            className="text-transparent bg-clip-text bg-gradient-to-r via-purple-400 to-pink-500"
+                        >
+                            Hello, I'm{" "}
+                        </span>
+                        Davod
+                    </h1>
+                    <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
+                        I build things for the web ekfhwgietgwejgtw
+                        hieibns  rwnio ntg ni puhtgthgptutrwepn iour
+                    </p>
+                    <div>
+                        <button
+                            className="px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white"
+                        >
+                            Hire me
+                        </button>
+                        <button
+                            className="px-1 py-1 rounded-full bg-transparent w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white border border-white mt-3"
+                        >
+                            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                                Contact me
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+                    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                        <Image
+                            src="/images/hero-image.png"
+                            alt="Hero Image"
+                            width={300}
+                            height={300}
+                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                        />
+                    </div>
+                </div>
             </div>
-            <div className="col-span-5">
-                <Image
-                    src="/images/hero-image.png"
-                    alt="Hero Image"
-                    width={500}
-                    height={500}
-                    className="w-full h-auto"
-                />
-            </div>
-        </div>
-    </section>
-  );
+                </section>
+    );
 };
 
 export default HeroSection;
