2ed5a51e1f2fb4f754442a61437fbbfd530f8cce
Davod <davodnikoyii@gmail.com>
2025-09-16 00:07:23 +0200

    Add AchievementsSection component and update HeroSection and Navbar for improved layout and animations; configure Tailwind CSS

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,62 +1,65 @@
 "use client";
-import React, { useState } from "react";
 import Link from "next/link";
+import React, { useState } from "react";
 import NavLink from "./NavLink";
-import { BeakerIcon, Bars3Icon  } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
-    {
-        title: "About",
-        path: "#about",
-    },
-    {
-        title: "Projects",
-        path: "#projects"
-    },
-    {
-        title: "Contact",
-        path: "#contact"
-    }
+  {
+    title: "About",
+    path: "#about",
+  },
+  {
+    title: "Projects",
+    path: "#projects",
+  },
+  {
+    title: "Contact",
+    path: "#contact",
+  },
 ];
 
-
 const Navbar = () => {
-    const [navBarOpen, setNavBarOpen] = useState(false);
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-<nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
-        <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
-            <Link className="text-2 md:text-5xl text-white font-semibold" href={"/"}>
-                LOGO
-            </Link>
-            <div className="mobile-menu block md:hidden">
-                {!navBarOpen ? (
-                    <button onClick={() => setNavBarOpen(true)} className="text-slate-200 flex items-center px-3 py-2 border rounded border-slate-200 hover:text-white hover:border-white">
-                        <Bars3Icon className="h-5 w-5"></Bars3Icon>
-                    </button>
-                ) : (
-                    <button onClick={() => setNavBarOpen(false)} className="text-slate-200 flex items-center px-3 py-2 border rounded border-slate-200 hover:text-white hover:border-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                        </svg>
-
-                    </button>
-                )}
-            </div>
-            <div className="menu hidden md:block md:w-auto" id="navbar">
-                <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-10">
-                    {navLinks.map((link, index) => (
-                        <li key={index}>
-                            <NavLink href={link.path} title={link.title} />
-                        </li>
-                    ))}
-                </ul>
-            </div>
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
+          LOGO
+        </Link>
+        <div className="mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <Bars3Icon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <XMarkIcon className="h-5 w-5" />
+            </button>
+          )}
         </div>
-        {navBarOpen ? (
-            <MenuOverlay links={navLinks} />
-        ) : null}
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
     </nav>
   );
 };
