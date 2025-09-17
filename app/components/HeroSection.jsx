803f1cd4abf32eb56fc693cf154a6f9add32f940
Davod <davodnikoyii@gmail.com>
2025-09-17 09:12:43 +0200

    Integrate TypingLoop component into HeroSection

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import TypingLoop from "./TypingLoop";
 
 const HeroSection = () => {
   return (
@@ -15,27 +16,12 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+          <h1 className="text-white text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m{" "}
             </span>
-            <br></br>
-            <TypeAnimation
-              sequence={[
-                "Judy",
-                1000,
-                "a Web Developer",
-                1000,
-                "a Mobile Developer",
-                1000,
-                "a UI/UX Designer",
-                1000,
-              ]}
-              wrapper="span"
-              speed={50}
-              repeat={Infinity}
-            />
           </h1>
+            <TypingLoop text1={"Davod"} text2={"a system developer"} text3={"also a mobile and web application developer"} />
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
             voluptuous.
--- app/components/TypingLoop.jsx
+++ app/components/TypingLoop.jsx
@@ -3,20 +3,23 @@ import { TypeAnimation } from 'react-type-animation';
 
 const TypingLoop= ({ text1, text2, text3 }) => {
   return (
-    <TypeAnimation className="text-white text-4xl sm:text-5xl lg:text-6xl font-extrabold mt-5"
-      sequence={[
-        text1,
-        1000,
-        text2,
-        1000,
-        text3,
-        1000,
-      ]}
-      wrapper="span"
-      speed={50}
-      style={{ fontSize: '2em', display: 'inline-block' }}
-      repeat={Infinity}
-    />
+    <div className="text-white text-1xl sm:text-2xl lg:text-4xl font-extrabold mb-6">
+      <TypeAnimation
+        sequence={[
+          text1,
+          1000,
+          text2,
+          1000,
+          text3,
+          1000,
+        ]}
+        wrapper="span"
+        speed={50}
+        style={{ fontSize: '2em', display: 'inline-block' }}
+        repeat={Infinity}
+      />
+    </div>
+
   );
 };
 export default TypingLoop;
