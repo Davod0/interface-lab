ef17a9e79bdc1ba2365a943094b20525806870f2
Davod <davodnikoyii@gmail.com>
2025-09-17 23:11:16 +0200

    Refactor POST function in route.js to include response parameter and improve error handling

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -4,14 +4,14 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req) {
+export async function POST(req, res) {
   const { email, subject, message } = await req.json();
-
+  console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
       from: fromEmail,
       to: [fromEmail, email],
-      subject,
+      subject: subject,
       react: (
         <>
           <h1>{subject}</h1>
@@ -21,9 +21,8 @@ export async function POST(req) {
         </>
       ),
     });
-
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json({ error });
   }
 }
