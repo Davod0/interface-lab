a827838df439a90d1f2dce720f01afc69027d084
Davod <davodnikoyii@gmail.com>
2025-09-17 23:00:08 +0200

    Refactor POST function in route.js to remove unused parameters and improve error handling

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -4,14 +4,14 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
+export async function POST(req) {
   const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
+
   try {
     const data = await resend.emails.send({
       from: fromEmail,
       to: [fromEmail, email],
-      subject: subject,
+      subject,
       react: (
         <>
           <h1>{subject}</h1>
@@ -21,8 +21,9 @@ export async function POST(req, res) {
         </>
       ),
     });
+
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json({ error });
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
