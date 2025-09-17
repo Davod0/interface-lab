a3c21cddf3457f5f85b076fc60227dfda193dd2e
Davod <davodnikoyii@gmail.com>
2025-09-17 15:26:55 +0200

    Refactor code structure for improved readability and maintainability

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -51,7 +51,6 @@ const HeroSection = () => {
               Contact Me
             </Link>
           </div>
-
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
@@ -59,15 +58,15 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="/images/hero-image.png"
-              alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
-            />
-          </div>
+        <div className="rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <Image
+            src="/images/pc3.png"
+            alt="hero image"
+            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            width={300}
+            height={300}
+          />
+        </div>
         </motion.div>
       </div>
     </section>
