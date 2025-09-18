76b9886b7cd72ed292e538d66b985853226e005d
Davod <davodnikoyii@gmail.com>
2025-09-18 13:09:13 +0200

    Refactor EmailSection and MenuOverlay components for improved structure and styling

--- app/components/MenuOverlay.jsx
+++ app/components/MenuOverlay.jsx
@@ -3,23 +3,25 @@ import React from "react";
 
 const MenuOverlay = ({ links, handleScroll }) => {
   return (
-    <ul className="flex flex-col py-3 items-center space-y-2">
-      {links.map((link, index) => (
-        <li key={index}>
-          <button
-            onClick={() => handleScroll(link.path)}
-            className="
-              text-[#ADB7BE] font-medium px-3 py-1 text-sm sm:text-base
-              transition-all duration-300 ease-in-out
-              hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r
-              hover:from-primary-400 hover:to-secondary-600 cursor-pointer
-            "
-          >
-            {link.title}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <div className="absolute top-full left-0 w-full bg-gradient-to-r from-[#18181b] via-[#121212] to-[#23272e] z-40 border-t border-[#23272e] shadow-lg">
+      <ul className="flex flex-col py-3 items-center space-y-2">
+        {links.map((link, index) => (
+          <li key={index}>
+            <button
+              onClick={() => handleScroll(link.path)}
+              className="
+                text-[#ADB7BE] font-medium px-3 py-1 text-sm sm:text-base
+                transition-all duration-300 ease-in-out
+                hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r
+                hover:from-primary-400 hover:to-secondary-600 cursor-pointer
+              "
+            >
+              {link.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
