b26cb1f5c482441881f57071cf6b0cde4c65a1f4
Davod <davodnikoyii@gmail.com>
2025-09-18 10:34:08 +0200

    Update Navbar component styles and adjust image positioning

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -18,14 +18,14 @@ const Navbar = () => {
   return (
     <nav className="fixed mx-auto border border-[#23272e] top-0 left-0 right-0 z-10
       bg-gradient-to-r from-[#18181b] via-[#121212] to-[#23272e] bg-opacity-90 shadow-lg backdrop-blur-lg">
-      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+      <div className="flex w-full flex-wrap items-center justify-between lg:py-4 py-2 px-1">
         <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold flex items-center gap-1">
           <Image
             src="/images/dav.jpg"
             alt="hero image"
             width={40}
             height={60}
-            className="rounded-full object-cover"
+            className="rounded-full object-cover ml-5 lg:ml-15"
           />
         </Link>
         <div className="mobile-menu block md:hidden">
@@ -46,22 +46,22 @@ const Navbar = () => {
           )}
         </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-4 mt-0">
             {navLinks.map((link, index) => (
               <li key={index} className="relative group">
                 <Link
                   href={link.path}
                   className="
                     text-[#ADB7BE] font-semibold px-4 py-2 transition-all duration-300 ease-in-out
-                    hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r 
+                    hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r
                     hover:from-primary-400 hover:to-secondary-600
                   "
                 >
                   {link.title}
                   <span
                     className="
-                      block absolute left-1/2 -translate-x-1/2 -bottom-1 h-0.5 w-0 
-                      group-hover:w-full bg-gradient-to-r from-primary-400 to-secondary-600 
+                      block absolute left-1/2 -translate-x-1/2 -bottom-1 h-0.5 w-0
+                      group-hover:w-full bg-gradient-to-r from-primary-400 to-secondary-600
                       rounded-full transition-all duration-300
                     "
                   ></span>
