c8ed5947bda47d263295fcffc4ec139fdd4809cc
Davod <davodnikoyii@gmail.com>
2025-09-20 12:20:42 +0200

    Add SVG icons for various technologies in the tech stack

--- app/components/TechStack.jsx
+++ app/components/TechStack.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import sqlDatabase from "../../public/images/sqlDatabase.svg";
-import nuxtjs from "../../public/images/nuxtjs.svg";
 
-
-// const techIcons = [
-//   { src: sqlDatabase, alt: "SQL Database Icon" },
-//   { src: nuxtjs, alt: "Next.js Icon" },
-// ];
-
-const techIcons = Array.from({ length: 20 }, () => ({
-  src: sqlDatabase,
-  alt: "Tech Stack Icon",
-}));
+const techIcons = [
+  { src: "/tech-stack/C.svg", alt: "C#" },
+  { src: "/tech-stack/Docker.svg", alt: "Docker" },
+  { src: "/tech-stack/Firebase.svg", alt: "Firebase" },
+  { src: "/tech-stack/Git.svg", alt: "Git" },
+  { src: "/tech-stack/GitLab.svg", alt: "GitLab" },
+  { src: "/tech-stack/Google Cloud.svg", alt: "Google Cloud" },
+  { src: "/tech-stack/JavaScript.svg", alt: "JavaScript" },
+  { src: "/tech-stack/MongoDB.svg", alt: "MongoDB" },
+  { src: "/tech-stack/Nest.js.svg", alt: "Nest.js" },
+  { src: "/tech-stack/NET.svg", alt: ".NET" },
+  { src: "/tech-stack/Nuxt JS.svg", alt: "Nuxt JS" },
+  { src: "/tech-stack/Postman.svg", alt: "Postman" },
+  { src: "/tech-stack/Python.svg", alt: "Python" },
+  { src: "/tech-stack/React.svg", alt: "React" },
+  { src: "/tech-stack/Redux.svg", alt: "Redux" },
+  { src: "/tech-stack/SQLite.svg", alt: "SQLite" },
+  { src: "/tech-stack/Storybook.svg", alt: "Storybook" },
+  { src: "/tech-stack/Tailwind CSS.svg", alt: "Tailwind CSS" },
+  { src: "/tech-stack/TypeScript.svg", alt: "TypeScript" },
+  { src: "/tech-stack/Vue.js.svg", alt: "Vue.js" },
+  { src: "/tech-stack/SQL Database.svg", alt: "SQL Database" },
+  { src: "/tech-stack/HTML5.svg", alt: "HTML" },
+  { src: "/tech-stack/CSS3.svg", alt: "CSS" },
+  { src: "images/github-icon.svg", alt: "Github" },
+  { src: "/tech-stack/Ubuntu.svg", alt: "Ubuntu" }
+];
 
 const TechStack = () => {
   return (
@@ -25,11 +40,16 @@ const TechStack = () => {
         {techIcons.map((icon, i) => (
           <div
             key={i}
-            className={"shadow rounded-lg aspect-[4/3] flex items-center justify-center bg-white"}
+            className="group shadow rounded-lg aspect-[4/3] flex items-center justify-center"
           >
-            <span className="text-xs font-semibold flex flex-col items-center">
-              <Image src={icon.src} alt={icon.alt} />
-              <p className="text-black">SQL Database</p>
+            <span className="flex flex-col items-center cursor-pointer">
+              <Image src={icon.src} alt={icon.alt} width={48} height={48} />
+              <p
+                className="opacity-0 group-hover:opacity-100 transition-opacity
+                duration-300 text-white mt-2 md:mt-3 text-[10px] md:text-xs font-semibold"
+              >
+                {icon.alt}
+              </p>
             </span>
           </div>
         ))}
