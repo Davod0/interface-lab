e6f03fd60da09650943e67a5bfca8a42c27d993c
Davod <davodnikoyii@gmail.com>
2025-10-01 14:55:44 +0200

    Refactored route.js

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,47 +1,23 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
-// const fromEmail = process.env.FROM_EMAIL;
-
-// export async function POST(req, res) {
-//   const { email, subject, message } = await req.json();
-//   console.log(email, subject, message);
-//   try {
-//     const data = await resend.emails.send({
-//       from: fromEmail,
-//       to: [fromEmail, email],
-//       subject: subject,
-//       react: (
-//         <>
-//           <h1>{subject}</h1>
-//           <p>Thank you for contacting us!</p>
-//           <p>New message submitted:</p>
-//           <p>{message}</p>
-//         </>
-//       ),
-//     });
-//     return NextResponse.json(data);
-//   } catch (error) {
-//     return NextResponse.json({ error });
-//   }
-// }
-
-// export async function GET() {
-//   return NextResponse.json({ message: "Hello world" }); onboarding@resend.dev
-
-// }
-
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function GET() {
+export async function POST(req) {
+  const { email, subject, message } = await req.json();
   try {
     const data = await resend.emails.send({
       from: process.env.FROM_EMAIL,
       to: process.env.TO_EMAIL,
-      subject: "Hello test",
-      html: "<h1>Hello test mmmm</h1>",
+      subject: subject,
+      react: (
+              <>
+                <h1>{subject}</h1>
+                <p>New message sent by: {email}</p>
+                <p>{message}</p>
+              </>
+            ),
     });
 
     return NextResponse.json(data);
